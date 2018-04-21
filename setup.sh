398deca5e28bc2b91a6d9f6d210046b527db46d5
WrBug <i@wrbug.com>
2018-04-21 11:22:55 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz
+tar -xvJf  Python-3.6.2.tar.xz
+cd Python-3.6.2
+./configure prefix=/usr/local/python3
+make && make install
+ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
