5b078f9b067104b816e6e0dab6c2944845cf74e3
WrBug <i@wrbug.com>
2021-03-01 16:16:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 wget https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tar.xz
 tar -xvJf  Python-3.9.2.tar.xz
-cd Python-3.9.2.tar.xz
+cd Python-3.9.2
 ./configure prefix=/usr/local/python3
 make && make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
